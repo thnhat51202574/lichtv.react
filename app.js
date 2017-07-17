64273404799b942a7f1ef72b27f7473021b12b10
Lich Truong <lichcse@gmail.com>
2017-07-17 22:54:04 +0700

    start refresh

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jsx');
-var options = { beautify: true };
-app.engine('jsx', require('express-react-views').createEngine(options));
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -29,20 +27,20 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const util = require('util');
-
-function requireAuthentication(req, res, next) {
-    res._data_user = { name: "Lich Truong", id: 1 };
-    console.log("requireAuthentication");
-    return next();
-}
-
-function loadUser(req, res, next) {
-    res._data_user.login = "lichcse";
-    console.log("loadUser");
-    return next();
-}
-
 /* GET users listing. */
-router.get('/', requireAuthentication, loadUser, function(req, res, next) {
-    console.log("User page");
-    console.log(res._data_user);
-    res.send('respond with a resource');
-});
-
-router.get('/lichtv', function(req, res, next) {
-    console.log("LichTV");
-    res.send('respond with a resource');
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
 
 module.exports = router;
