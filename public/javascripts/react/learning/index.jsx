fb54d61b0e5e89cf6b0044e6f2f963ef87cccc07
Lich Truong <lichcse@gmail.com>
2017-07-18 23:46:34 +0700

    list dynamic rendering

--- public/javascripts/react/learning/index.jsx
+++ public/javascripts/react/learning/index.jsx
@@ -1,29 +1,51 @@
-function Blog(props) {
-    const posts = props.posts;
-    return (
-        <div>
-            <ul>
+class Blog extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {posts : this.props.posts};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.setState({}),
+            1000
+        );
+    }
+
+    render() {
+        const posts = this.state.posts;
+        return (
+            <div>
+                <ul>
+                    {posts.map((post) =>
+                        <li key={post.id}>
+                            {post.title}
+                        </li>
+                    )}
+                </ul>
+                <hr />
                 {posts.map((post) =>
-                    <li key={post.id}>
-                        {post.title}
-                    </li>
+                    <div key={post.id}>
+                        <h3>{post.title}</h3>
+                        <p>{post.content}</p>
+                    </div>
                 )}
-            </ul>
-            <hr />
-            {posts.map((post) =>
-                <div key={post.id}>
-                    <h3>{post.title}</h3>
-                    <p>{post.content}</p>
-                </div>
-            )}
-        </div>
-    );
+            </div>
+        );
+    }
 }
 
-const posts = [
+var posts = [
     {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
     {id: 2, title: 'Installation', content: 'You can install React from npm.'}
 ];
+
+var iCount = 3;
+setInterval(function(){
+    posts.push({id: iCount, title: 'Blog ' + iCount, content: 'Count ' + iCount});
+    iCount++;
+},1000);
+
 ReactDOM.render(
     <Blog posts={posts} />,
     document.getElementById('root')
--- public/javascripts/react/learning/lists_keys.jsx
+++ public/javascripts/react/learning/lists_keys.jsx
@@ -181,3 +181,57 @@ ReactDOM.render(
     <Blog posts={posts} />,
     document.getElementById('root')
 );
+
+// example 09
+class Blog extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {posts : this.props.posts};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.setState({}),
+            1000
+        );
+    }
+
+    render() {
+        const posts = this.state.posts;
+        return (
+            <div>
+                <ul>
+                    {posts.map((post) =>
+                        <li key={post.id}>
+                            {post.title}
+                        </li>
+                    )}
+                </ul>
+                <hr />
+                {posts.map((post) =>
+                    <div key={post.id}>
+                        <h3>{post.title}</h3>
+                        <p>{post.content}</p>
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
+
+var posts = [
+    {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+    {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+];
+
+var iCount = 3;
+setInterval(function(){
+    posts.push({id: iCount, title: 'Blog ' + iCount, content: 'Count ' + iCount});
+    iCount++;
+},1000);
+
+ReactDOM.render(
+    <Blog posts={posts} />,
+    document.getElementById('root')
+);
