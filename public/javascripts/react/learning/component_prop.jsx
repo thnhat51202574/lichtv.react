5e8758580cd627359c5f9b7cafc16707ba10a8db
lichtv <lichtv@younetco.com>
2017-07-18 18:16:15 +0700

    state & lifecycle

--- public/javascripts/react/learning/component_prop.jsx
+++ public/javascripts/react/learning/component_prop.jsx
@@ -26,7 +26,7 @@ ReactDOM.render(
 
 // example 02
 function formatDate(date) {
-  return date.toLocaleDateString();
+    return date.toLocaleDateString();
 }
 
 function Comment(props) {
@@ -47,16 +47,72 @@ function Comment(props) {
                 {formatDate(props.date)}
             </div>
         </div>
-  ) ;
+    ) ;
 }
 
 const comment = {
     date: new Date(),
     text: 'I hope you enjoy learning React!',
     author: {
-        name: 'Hello Kitty',
-        avatarUrl: 'http://placekitten.com/g/64/64'
-  }
+        name: 'Hello LichTV',
+        avatarUrl: 'http://dev.younetsi.com/upload/resize_cache/main/251/100_100_2/3036714_orig.jpg'
+    }
+};
+
+ReactDOM.render(
+    <Comment
+        date={comment.date}
+        text={comment.text}
+        author={comment.author} />,
+    document.getElementById('root')
+);
+
+// example 03
+function formatDate(date) {
+    return date.toLocaleDateString();
+}
+
+function Avatar(props) {
+    return (
+        <img className="Avatar"
+            src={props.user.avatarUrl}
+            alt={props.user.name}
+        />
+    );
+}
+
+function UserInfo(props) {
+    return (
+        <div className="UserInfo">
+            <Avatar user={props.user} />
+            <div className="UserInfo-name">
+                {props.user.name}
+            </div>
+        </div>
+    );
+}
+
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <UserInfo user={props.author} />
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
+        </div>
+    );
+}
+
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+        name: 'Hello LichTV',
+        avatarUrl: 'http://dev.younetsi.com/upload/resize_cache/main/251/100_100_2/3036714_orig.jpg'
+    }
 };
 
 ReactDOM.render(
--- public/javascripts/react/learning/state_lifesycle.jsx
+++ public/javascripts/react/learning/state_lifesycle.jsx
@@ -1,3 +1,43 @@
+// example 01
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+);
+
+// example 02
 class Clock extends React.Component {
     constructor(props) {
         super(props);
