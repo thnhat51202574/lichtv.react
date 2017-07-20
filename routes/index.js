063e7e59636baf7b2241a93abb4ca59434a8c4a8
lichtv <lichtv@younetco.com>
2017-07-20 13:36:14 +0700

    react tutorial

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-var Test = require('G:/LichTV/NodeJS/Project/lichtv.react/classes/test.js');
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    console.log("rootPathApp: ", rootPathApp);
-    Test.hello();
     res.render('index', { title: 'React Tutorial' });
 });
 
